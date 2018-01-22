c125f0d4713a17398906d46bd51c828423ef83f5
Tobias Hennig <hennig.tobias@guj.de>
2018-01-22 10:12:43 +0100

    Make task async

--- tasks/amd_bundle_report.js
+++ tasks/amd_bundle_report.js
@@ -12,15 +12,19 @@ var Reporter = require('amd-bundle-report')
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('amd_bundle_report', 'The best Grunt plugin ever.', function() {
-
+    var done = this.async();
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       limit: null
     });
 
-    var reports = Reporter.reportFiles(this.filesSrc, options);
-
-    grunt.log.write(reports.message);
+    Reporter.reportFiles(this.filesSrc, options).then(function(reports) {
+      grunt.log.write(reports.message);
+      done();
+    }).catch(function(err) {
+      grunt.log.error(err);
+      done(false);
+    });
   });
 
 };
